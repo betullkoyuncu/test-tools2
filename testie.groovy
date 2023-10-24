485191b7036b97968b8e3725d7b7a3c080001606
betullkoyuncu <117205036+betullkoyuncu@users.noreply.github.com>
2023-10-24 16:39:58 +0300

    Update testie.groovy

--- testie.groovy
+++ testie.groovy
@@ -2,6 +2,11 @@ class HelloWorld {
     public static void main(String[] args) {
         def a = '' + "Hello World"
         println(a.substring(0));
+
+        def xAdA = 104
+        println xAdA.getClass()
+        def x_A_f_AA = "Guru99"
+        println x_A_f_AA.getClass()
     }
     
 }
